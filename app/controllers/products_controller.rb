526b404ce60e843b24729f2d65bb7434092df6ee
Jay Deverett <jayjdeverett@gmail.com>
2017-08-06 20:44:05 -0400

    add new and create methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,10 +9,23 @@ class ProductsController < ApplicationController
   end
 
   def new
-
+    @product = Product.new
   end
 
   def create
+    @product = Product.new
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      flash[:notice] = "Product successfully created"
+      redirect_to '/products'
+
+    else
+      render :new
+    end
 
   end
 
