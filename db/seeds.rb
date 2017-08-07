26f2c91fefff0aaaea07e6d1ba4807fef4416cf0
Jay Deverett <jayjdeverett@gmail.com>
2017-08-06 20:29:25 -0400

    add show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  Product.new(name: "Injustice 2", description: "DC Superhero Fighting Game", price_in_cents: 8999)
-  Product.new(name: "Horizon Zero Dawn", description: "Futuristic open-world RPG", price_in_cents: 8999)
-  Product.new(name: "Overwatch", description: "First person shooter", price_in_cents: 8999)
-  Product.new(name: "FIFA 2017", description: "#1 Hit Soccer Game", price_in_cents: 8999)
+products  = Product.create([
+  {name: "Injustice 2", description: "DC Superhero Fighting Game", price_in_cents: 8999},
+  {name: "Horizon Zero Dawn", description: "Futuristic open-world RPG", price_in_cents: 8999},
+  {name: "Overwatch", description: "First person shooter", price_in_cents: 8999},
+  {name: "FIFA 2017", description: "#1 Hit Soccer Game", price_in_cents: 8999}
+])
